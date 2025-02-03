7b024ceeb58b08abe86c069a22991064bfe9c8a6
sebastian_foth <master_rg@t-online.de>
2025-02-03 18:52:57 +0100

    Extending PlayerstatisticsElement to save more data

--- src/Scripts/Game/Classes/AS_PlayerStatisticsElement.c
+++ src/Scripts/Game/Classes/AS_PlayerStatisticsElement.c
@@ -1,8 +1,29 @@
-class AS_PlayerStatsElement
+class AS_PlayerStatisticsElement
 {
     string m_sPlayerIdentityId;
 	
 	float m_fGamingTime;
+	
+	float m_fExperiencePoints;
+	
+	float m_fPointsInfantry;
+	float m_fPointsLogistics;
+	float m_fPointsMedical;
+	float m_fPointsWarcriminal;
+	
+	float m_fKillsEnemy;
+	float m_fKillsEnemyAI;
+	
+	float m_fKillsFriendly;
+	float m_fKillsFriendlyAI;
+	
+	float m_fRoadKillsEnemy;
+	float m_fRoadKillsEnemyAI;
+	
+	float m_fRoadKillsFriendly;
+	float m_fRoadKillsFriendlyAI;
+	
+	float m_fDeaths;
 
     float m_fDistanceWalked;
     float m_fDistanceDriven;
@@ -24,15 +45,85 @@ class AS_PlayerStatsElement
     float m_fMorphineFriendlies;
 
     //------------------------------------------------------------------------------------------------
-    void AS_PlayerStatsElement()
+    void AS_PlayerStatisticsElement()
     {
 
     }
+	
+	void SetRoadKillsFriendly(float f)
+	{
+		m_fRoadKillsFriendly = f;
+	}
+	
+	void SetRoadKillsFriendlyAI(float f)
+	{
+		m_fRoadKillsFriendlyAI = f;
+	}
+	
+	void SetRoadKillsEnemy(float f)
+	{
+		m_fRoadKillsEnemy = f;
+	}
+	
+	void SetRoadKillsEnemyAI(float f)
+	{
+		m_fRoadKillsEnemyAI = f;
+	}
+	
+	void SetDeaths(float f)
+	{
+		m_fDeaths = f;
+	}
+	
+	void SetKillsFriendly(float f)
+	{
+	  	m_fKillsFriendly = f;
+	}
+	
+	void SetKillsFriendlyAI(float f)
+	{
+	  	m_fKillsFriendlyAI = f;
+	}
+	
+	void SetKillsEnemy(float f)
+	{
+	  	m_fKillsEnemy = f;
+	}
+	
+	void SetKillsEnemyAI(float f)
+	{
+	  	m_fKillsEnemyAI = f;
+	}
 
     void SetPlayerIdentityId(string s)
     {
         m_sPlayerIdentityId = s;
     }
+	
+	void SetExperience(float f)
+	{
+	    m_fExperiencePoints = f;
+	}
+	
+	void SetPointsInfantry(float f)
+	{
+	    m_fPointsInfantry = f;
+	}
+	
+	void SetPointsLogistics(float f)
+	{
+	    m_fPointsLogistics = f;
+	}
+	
+	void SetPointsMedical(float f)
+	{
+	    m_fPointsMedical = f;
+	}
+	
+	void SetPointsWarcriminal(float f)
+	{
+	    m_fPointsWarcriminal = f;
+	}
 	
 	void SetDistanceWalked(float f)
 	{
