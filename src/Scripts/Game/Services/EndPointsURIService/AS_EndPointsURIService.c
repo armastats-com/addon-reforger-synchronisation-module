e898f557f9f958024358bce64a05c09372e0ef34
sebastian_foth <master_rg@t-online.de>
2025-02-04 17:58:56 +0100

    Added endpoint for server statistics

--- src/Scripts/Game/Services/EndPointsURIService/AS_EndPointsURIService.c
+++ src/Scripts/Game/Services/EndPointsURIService/AS_EndPointsURIService.c
@@ -3,6 +3,7 @@ class AS_EndPointURIService
 	protected const string m_sEndpointPing = "v1/receiver/ping";
 	protected const string m_sEndpointKillLog = "v1/receiver/kill-log";
 	protected const string m_sEndpointPlayerStatistics = "v1/receiver/player-statistics";
+	protected const string m_sEndpointServerStatistics = "v1/receiver/server-statistics";
 	
 	static string GetEndpointPing()
 	{
@@ -16,6 +17,11 @@ class AS_EndPointURIService
 	
 	static string GetEndPointPlayerStatistics()
 	{
-		return m_sEndpointKillLog;
+		return m_sEndpointPlayerStatistics;
+	}
+	
+	static string GetEndPointServerStatistics()
+	{
+		return m_sEndpointServerStatistics;
 	}
 }
